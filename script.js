af479a7f9301a6e506e1219a6eec2e19acbca0ec
aniketjadhav1812 <128202035+aniketjadhav1812@users.noreply.github.com>
2023-08-07 20:36:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ btn.addEventListener('click',toggleEyes);
 function toggleEyes(){
     if(btn.textContent.includes('उघड'))
     {
-        btn.textContent = "डोळे बंद कर लवड्या.";
+        btn.textContent = "डोळे बंद कर ❤️dy..";
         img.src = "2.jpg";
     }
     else{
-        btn.textContent = "डोळे उघड लवड्या.";
+        btn.textContent = "डोळे उघड ❤️dy..";
         img.src = "1.jpg";
     }
 }
